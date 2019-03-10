1b57951d6b7d192aa2a7b76ab9838a4cbd2a0b56
nayramohamed <nayramohamed706@gmail.com>
2019-03-10 20:21:06 +0200

    Add utill

--- src/ass2/Block.java
+++ src/ass2/Block.java
@@ -19,5 +19,14 @@ public class Block {
 			this.timeStamp = new Date().getTime();
 			
                 }		}
+public static String applySha256(String input){
+			
+			try {
+				MessageDigest digest = MessageDigest.getInstance("SHA-256");
+		        
+				//Applies sha256 to our input, 
+				byte[] hash = digest.digest(input.getBytes("UTF-8"));
+		        
+				StringBuffer hexString = new StringBuffer(); // This will contain hash as hexidecimal
                  
 } 
